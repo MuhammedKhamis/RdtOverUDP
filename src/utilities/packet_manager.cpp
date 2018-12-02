8a92250c74a6f537fc31455c981b52abb77999e9
abdo <abdoyasser702@gmail.com>
2018-12-02 20:16:50 +0200

    "implementing parser"

--- src/utilities/packet_manager.cpp
+++ src/utilities/packet_manager.cpp
@@ -5,28 +5,6 @@
 #include "packet_parser.h"
 #include <boost/lexical_cast.hpp>
 
-// -- Data in packets structure
-
-// -----------------------------------------
-//
-//   -Checksum : number\r\n
-//   -Length : number\r\n
-//   -Seq_number : number\r\n
-//   -Last_packet : 0/1\r\n\r\n
-//   -Data : \r\n
-//   -Line 1 data
-//   -Line 2 data
-//          .
-//          .
-//          .
-//          .
-// -----------------------------------------
-
-
-
-
-
-
 string packet_manager::assemble_data(vector<packet> packet_queue) {
   throw std::invalid_argument( "Not implemented" );
 }
--- src/utilities/packet_parser.cpp
+++ src/utilities/packet_parser.cpp
@@ -3,6 +3,22 @@
 #include <data_packet.h>
 #include "packet_parser.h"
 
+// -- Data in packets structure
+
+// -----------------------------------------
+//
+//   -Checksum : number\r\n
+//   -Length : number\r\n
+//   -Seq_number : number\r\n
+//   -Last_packet : 0/1\r\n\r\n
+//   -Data : \r\n
+//   -Line 1 data
+//   -Line 2 data
+//          .
+//          .
+//          .
+//          .
+// -----------------------------------------
 
 /* interface methods */
 /******************************************/
