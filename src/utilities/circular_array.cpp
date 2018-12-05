cdd4dab736d9bcb363fb03a109aff6d4baac126f
abdo <abdoyasser702@gmail.com>
2018-12-05 23:45:17 +0200

    implementing circular-array

--- src/utilities/circular_array.cpp
+++ src/utilities/circular_array.cpp
@@ -5,8 +5,9 @@
 circular_array::circular_array(int size)
 {
 	this->data_array(size);
-	start = 0;
-	end = 0;
+	this->start = 0 ;
+	this->end = 0 ;
+	this->size = 0 ;
 }
 
 /* interface methods */
@@ -14,28 +15,64 @@ circular_array::circular_array(int size)
 int
 circular_array::insert(struct packet_info pkt)
 {
-	if(is_full() == 1){return 0;}
-	data_array[end] = pkt;
-	if(end == data_array.length() - 1)
-	{
-		end = 0;
-		return 1;
+	if(is_full() == 1){
+		return -1 ;
 	}
-	end++;
+	data_array[end] = pkt;
+
+	end++ ;
+	size++ ;
+	end %= data_array.size() ;
+
 	return 1;
 }
 
-void
+int
 circular_array::mark_acked(int seq_no)
 {
+	int index = seq_no % data_array.size() ;
+	int total_ack = 1 ;
 
+	data_array[index].acked = True ;
+
+	if(index == start) {
+		while(data_array[index].acked){
+			index += 1 ;
+			index %= n ;
+			total_ack ++ ;
+		}
+		start = index ;
+	}
+
+	size -= total_ack ;
+
+	return total_ack ;
 }
 
 int
 circular_array::is_full()
 {
-
+	this->size == data_array.size() ;
 }
 
 vector<packet_info>::iterator
-circular_array::get_data_array();
+circular_array::begin(){
+	return data_array.begin() ;
+}
+
+void circular_array::update_array(int size){
+	vector<packet_info> db_array(size) ;
+	int i = start , j = start , siz = 0 ;
+
+	while(i < end && siz < size) {
+		db_array[j] = data_array[i] ;
+		i++ ;
+		j++ ;
+		siz ++ ;
+		i %= data_array.size() ;
+		j %= db_array.size() ;
+	}
+
+	data_array = db_array ;
+}
+
--- src/utilities/circular_array.h
+++ src/utilities/circular_array.h
@@ -13,7 +13,7 @@ struct packet_info {
 	int seq_no; // packet seq-no
 	int acked; // boolean 0,1
 	time_t start_time; // start time (timer) for this packet
-}
+};
 
 /* class definition */
 /******************************************/
@@ -22,8 +22,8 @@ class circular_array
 	private: 
 		// attributes
 		vector<struct packet_info> data_array;
-		int start;
-		int end;
+		int start = 0 ;
+		int end = 0 ;
 		int size;
 
 		// utility methods
@@ -34,9 +34,10 @@ class circular_array
 
 		// interface methods
 		int insert(struct packet_info pkt); // insert new packets
-		void mark_acked(int seq_no); // marks certain packet as acknowledged
-		void update_size(int new_size); // update window size
-		vector<packet_info>::iterator begin(); // returns iterator
+		int mark_acked(int seq_no); // marks certain packet as acknowledged
+		void update_array(int) ; // update window size
+        vector<packet_info>::iterator begin(); // returns iterator
+        bool is_full() ;
 };
 
 #endif 
